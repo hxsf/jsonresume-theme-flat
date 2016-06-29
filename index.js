668f8dd7a32406e815b27410e40777eed5ff5b6a
hxsf <hxsf@vip.qq.com>
2016-06-29 23:19:10 +0800

    support i18n

--- index.js
+++ index.js
@@ -6,8 +6,13 @@ module.exports = {
 };
 
 function render(resume) {
+	i18n = resume.i18n || [];
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+
+	Handlebars.registerHelper("i18n", function(value) {
+		return i18n[value] || value;
+	});
 	return Handlebars.compile(template)({
 		css: css,
 		resume: resume
